9c28df045246d1d3fbffa635b66cbf8b433a487b
yogeshprasai <yogeshprasai@gmail.com>
2024-08-17 16:32:13 -0400

    command line commit

--- angular/src/app/features/auth/sign-up/sign-up.component.ts
+++ angular/src/app/features/auth/sign-up/sign-up.component.ts
@@ -37,7 +37,7 @@ export class SignUpComponent  implements OnInit {
     console.log("hello 2");
   }
 
-  //Submit Creds also 2
+  //Submit Creds also 4
   submitCredentials(signUpForm: FormGroup){
     const email = signUpForm.get('email')?.value;
     const password = signUpForm.get('password')?.value;
