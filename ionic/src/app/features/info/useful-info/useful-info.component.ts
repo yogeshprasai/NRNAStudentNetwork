441b296f74bea402bb1c65e391c378cc99d3aad9
YogeshPrasai <pogeshprasai-macmini@Yogesh-Mac-Personal.local>
2025-04-21 11:42:26 -0400

    Content changes for few files

--- ionic/src/app/features/info/useful-info/useful-info.component.ts
+++ ionic/src/app/features/info/useful-info/useful-info.component.ts
@@ -7,9 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsefulInfoComponent implements OnInit {
 
-  readonly nrnaEmail: string = "students@nrnusa.org";
-
-  readonly nrnUSAEmail: string = "students@nrnusa.org";
+  readonly nrnUSAEmail: string = "info@nrnusa.org";
 
   constructor() { }
 
