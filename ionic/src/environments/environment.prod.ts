d37f61126312cfab1a15eb539c6082b94b61aea9
YogeshPrasai <pogeshprasai-macmini@Yogesh-Mac-Personal.local>
2025-05-17 17:47:29 -0400

    UI Bug fixes and adding android build

--- ionic/src/environments/environment.prod.ts
+++ ionic/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   //server_url: "http://localhost:8080",
-  server_url: "http://ec2-13-58-76-72.us-east-2.compute.amazonaws.com:8080",
+  server_url: "http://ec2-13-58-134-2.us-east-2.compute.amazonaws.com:8080",
 };
